6fcd2e39bbfb4cbdc725b72c915d201b55ff2d6d
facundo-cejas9 <facundo962010@hotmail.com>
2024-07-04 12:28:06 -0300

    change passwords

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
     try {
-        await mongoose.connect('mongodb+srv://alojen:aloja123@cluster0.eaizysy.mongodb.net/?', {       });
+        await mongoose.connect(process.env.DB_PASS, {       });
         console.log("DB connected");
     } catch (error) {
         console.error("Error connecting to database:", error);
